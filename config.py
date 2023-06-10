7965f38a4acc809b78c53ac43de7f66101b386ed
NorthernBlow <rheinhart@yandex.ru>
2023-06-10 13:17:48 +0300

    first fastapi commit

---
+++ config.py
@@ -0,0 +1,20 @@
+##########################################################################
+###                         config.py                                  ###
+###                                                                    ###
+###           This file represents config database                	   ###
+###     			                                                   ###
+##########################################################################
+
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+
+
+SQLALCHEMY_DATABASE_URL = 'postgresql://postnorthern:2191@localhost/task'
+
+engine = create_engine(SQLALCHEMY_DATABASE_URL)
+
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+Base = declarative_base()
+
